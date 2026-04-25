4fc261969f4b171faef825e57102f1bf2620a95f
JP094 <prakashjenish3@gmail.com>
2026-04-25 15:32:05 +0930

    Add files

---
+++ app/src/main/java/com/github/jenish094/p_stream_androidtv/mainActivity.kt
@@ -0,0 +1,42 @@
+package com.github.jenish094.p_stream_androidtv
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val allowedHost = "pstream.jenish.tech"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                val uri: Uri = request.url
+
+                return !uri.host.orEmpty().contains(allowedHost)
+            }
+        }
+
+        webView.loadUrl("https://pstream.jenish.tech")
+    }
+}
